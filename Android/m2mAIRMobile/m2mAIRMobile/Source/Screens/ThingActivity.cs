cb5103092bfca01fec0414ce1ecdbe6e9868f436
erankatzshallev <eran@kimaia.com>
2015-05-27 20:09:42 +0300

    MapFragment
    MapFragment

--- Android/m2mAIRMobile/m2mAIRMobile/Source/Screens/ThingActivity.cs
+++ Android/m2mAIRMobile/m2mAIRMobile/Source/Screens/ThingActivity.cs
@@ -10,18 +10,18 @@ using Android.Gms.Maps.Model;
 using Shared.ViewModel;
 using Shared.Model;
 using Shared.Utils;
+using Android.Views;
 
 namespace Android.Source.Screens
 {
 	[Activity]			
-	public class ThingActivity : BaseActivity, IOnMapReadyCallback
+	public class ThingActivity : BaseActivity, IOnMapReadyCallback, GoogleMap.IInfoWindowAdapter, GoogleMap.IOnInfoWindowClickListener 
 	{
 		private ThingViewModel 	viewModel;
 		private Thing 			daThing;
 
 		private NavigationBarView navBar; 
 		private ThingBriefDescriptionView thingBriefView;
-		private LocationTextView locationView;
 
 		private GoogleMap gMap;
 
@@ -32,8 +32,9 @@ namespace Android.Source.Screens
 			// Set our view from the "main" layout resource
 			SetContentView (m2m.Android.Resource.Layout.activity_thing);
 
+			SetUpMap ();
+
 			thingBriefView = FindViewById<ThingBriefDescriptionView>(m2m.Android.Resource.Id.ThingBriefDescriptionView);
-			locationView = FindViewById<LocationTextView>(m2m.Android.Resource.Id.LocationTextView);
 			navBar = FindViewById<NavigationBarView>(m2m.Android.Resource.Id.NavigationBarView); 
 			navBar.SetTitle("Thing");
 
@@ -53,9 +54,8 @@ namespace Android.Source.Screens
 					Logger.Debug ("OnDBLoadThingObject()");
 					daThing = viewModel.GetThing ();		
 					thingBriefView.SetThing (daThing);
-					locationView.SetLocation (daThing);
 
-					SetUpMap();
+					SetMapThing();
 				});
 			}
 			catch(Exception e){
@@ -63,25 +63,62 @@ namespace Android.Source.Screens
 			}
 		}
 
+		#region GoogleMap
 		private void SetUpMap()
 		{
-			if (gMap == null) 
-			{
-				(FragmentManager.FindFragmentById<MapFragment>(m2m.Android.Resource.Id.map)).GetMapAsync(this);
-			}
+			if (gMap == null)
+				(FragmentManager.FindFragmentById<MapFragment> (m2m.Android.Resource.Id.map)).GetMapAsync (this);
 		}
 
 		public void OnMapReady(GoogleMap gmap)
 		{
-			gMap = gmap;	
+			gMap = gmap;
 		}
+
+		private void SetMapThing()
+		{
+			LatLng latlng = new LatLng (daThing.loc.lat, daThing.loc.lng);
+			CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom (latlng, 10);
+			gMap.MoveCamera (camera);
+
+			MarkerOptions options = new MarkerOptions ().SetPosition (latlng).SetTitle (daThing.name).Draggable (true);
+			gMap.AddMarker (options);
+
+			gMap.SetInfoWindowAdapter (this);
+			gMap.SetOnInfoWindowClickListener (this);
+		}
+
+		public View GetInfoWindow (Marker marker)
+		{
+			View view = LayoutInflater.Inflate (m2m.Android.Resource.Layout.map_thing_info_window, null, false);
+			view.FindViewById<TextView> (m2m.Android.Resource.Id.ThingName).Text = daThing.name;
+			view.FindViewById<TextView>(m2m.Android.Resource.Id.Status).Text = (daThing.connected) ? "connected" : "disconnected";
+			view.FindViewById<TextView> (m2m.Android.Resource.Id.LastSeen).Text = daThing.lastSeen;
+			return view;
+		}
+
+		public View GetInfoContents (Marker marker)
+		{
+			return null;
+		}
+
+		void GoogleMap.IOnInfoWindowClickListener.OnInfoWindowClick (Marker marker)
+		{
+			OnProperties ();
+		}
+
+		public void OnMapClick (LatLng point)
+		{
+			OnProperties ();
+		}
+		#endregion
 
 		#region Event handlers
 		private void OnProperties()
 		{
 			var intent = new Intent(this, typeof(PropertiesListActivity));
 			intent.PutExtra (Shared.Model.Constants.DATA_MODEL_THING_KEY_IDENTIFIER, daThing.key);
-			Logger.Debug ("OnListItemClick() Thing key: " + daThing.key);
+			Logger.Debug ("OnProperties() Thing key: " + daThing.key);
 
 			StartActivity(intent);
 			Finish ();
