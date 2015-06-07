f3f8fc40afebb98d85f7d29095df8dc102cd49ba
erankatzshallev <eran@kimaia.com>
2015-06-07 10:18:22 +0300

    ChartsView
    ChartsView

--- Android/m2mAIRMobile/m2mAIRMobile/Source/Screens/PropertyActivity.cs
+++ Android/m2mAIRMobile/m2mAIRMobile/Source/Screens/PropertyActivity.cs
@@ -10,25 +10,28 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Shared.Utils;
-using Shared.Model;
 using Android.Source.Views;
 using Android.Source.Screens;
-using Shared.ViewModel;
 using NChart3D_Android;
 using Android.Graphics;
+
+using Shared.Utils;
+using Shared.Model;
+using Shared.ViewModel;
+using m2m.Android.Source.Views;
+
 
 namespace m2m.Android.Source.Screens
 {
 	[Activity]
-	public class PropertyActivity : BaseActivity, INChartSeriesDataSource
+	public class PropertyActivity : BaseActivity
 	{
 		private PropertyViewModel 			viewModel;
 		private Thing 						daThing;
 
 		private NavigationBarView 			navBar; 
 		private ThingBriefDescriptionView 	thingBriefView;
-		private NChartView 					chartView;
+		private ChartsView 					chartsView;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -45,20 +48,8 @@ namespace m2m.Android.Source.Screens
 			string tkey = Intent.GetStringExtra(Shared.Model.Constants.DATA_MODEL_THING_KEY_IDENTIFIER);
 			viewModel.GetThingObject (tkey, OnDBLoadThingObject, ShowDialog);
 
-			chartView = FindViewById<NChartView> (Resource.Id.nchartView);
-			LoadChartView ();
-		}
-
-		protected override void OnResume ()
-		{
-			base.OnResume ();
-			chartView.OnResume ();
-		}
-
-		protected override void OnPause ()
-		{
-			base.OnPause ();
-			chartView.OnPause ();
+			chartsView = FindViewById<ChartsView> (Resource.Id.chartsView);
+			chartsView.LoadChartView ();
 		}
 
 
@@ -70,10 +61,10 @@ namespace m2m.Android.Source.Screens
 					daThing = viewModel.GetThing ();		
 					thingBriefView.SetThing (daThing);
 
-					string pName = Intent.GetStringExtra(Shared.Model.Constants.DATA_MODEL_PROPERTY_NAME_IDENTIFIER);	
-					viewModel.GetPropertyHistory(pName, OnPropertyHistory, ShowDialog); 
-
-					StartLoadingSpinner("Collecting Propertie's records.");
+//					string pName = Intent.GetStringExtra(Shared.Model.Constants.DATA_MODEL_PROPERTY_NAME_IDENTIFIER);	
+//					viewModel.GetPropertyHistory(pName, OnPropertyHistory, ShowDialog); 
+//
+//					StartLoadingSpinner("Collecting Propertie's records.");
 
 					OnPropertyHistory();
 				});
@@ -87,43 +78,7 @@ namespace m2m.Android.Source.Screens
 		{
 			Logger.Debug ("OnPropertyHistory()");
 			// update chart
-			chartView.Chart.UpdateData ();
+			chartsView.Update();
 		}
-
-		#region NCharts
-		private void LoadChartView ()
-		{
-			// chart
-			chartView.Chart.LicenseKey = "";
-			chartView.Chart.CartesianSystem.Margin = new NChartMargin (10.0f, 10.0f, 10.0f, 20.0f);
-			chartView.Chart.ShouldAntialias = true;
-
-			// series
-			NChartColumnSeries series = new NChartColumnSeries ();
-			series.Brush = new NChartSolidColorBrush (Color.Red);
-			series.DataSource = this;
-			chartView.Chart.AddSeries (series);
-		}
-
-		// Get points for the series.
-		public NChartPoint[] Points (NChartSeries series)
-		{
-			Random random = new Random ();
-			NChartPoint[] result = new NChartPoint[11];
-			for (int i = 0; i <= 10; ++i)
-				result [i] = new NChartPoint (NChartPointState.PointStateAlignedToXWithXY (i, random.Next (30) + 1), series);
-			return result;
-		}
-		// Get name of the series.
-		public string Name (NChartSeries series)
-		{
-			return "My series";
-		}
-
-		public global::Android.Graphics.Bitmap Image (NChartSeries series)
-		{
-			return null;
-		}
-		#endregion
 	}
 }
---
+++ Android/m2mAIRMobile/m2mAIRMobile/Source/Views/ChartsView.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+using Android.Graphics;
+using NChart3D_Android;
+
+
+namespace Android.Source.Views
+{
+	public class ChartsView : LinearLayout, INChartSeriesDataSource
+	{
+		private NChartView		chartView;
+
+		public ChartsView (Context context) :
+			base (context)
+		{
+			Initialize ();
+		}
+
+		public ChartsView (Context context, IAttributeSet attrs) :
+			base (context, attrs)
+		{
+			Initialize ();
+		}
+
+		public ChartsView (Context context, IAttributeSet attrs, int defStyle) :
+			base (context, attrs, defStyle)
+		{
+			Initialize ();
+		}
+
+		void Initialize ()
+		{
+			LayoutInflater.From(Context).Inflate(m2m.Android.Resource.Layout.charts_view, this);
+			chartView = FindViewById<NChartView>(m2m.Android.Resource.Id.nchartView);
+		}
+
+
+		public void LoadChartView ()
+		{
+			// chart
+			chartView.Chart.LicenseKey = "";
+			chartView.Chart.CartesianSystem.Margin = new NChartMargin (10.0f, 10.0f, 10.0f, 20.0f);
+			chartView.Chart.ShouldAntialias = true;
+
+			// series
+			NChartColumnSeries series = new NChartColumnSeries ();
+			series.Brush = new NChartSolidColorBrush (Color.Red);
+			series.DataSource = this;
+			chartView.Chart.AddSeries (series);
+		}
+
+		public void Update()
+		{
+			chartView.Chart.UpdateData ();
+		}
+
+		// Get points for the series.
+		public NChartPoint[] Points (NChartSeries series)
+		{
+			Random random = new Random ();
+			NChartPoint[] result = new NChartPoint[11];
+			for (int i = 0; i <= 10; ++i)
+				result [i] = new NChartPoint (NChartPointState.PointStateAlignedToXWithXY (i, random.Next (30) + 1), series);
+			return result;
+		}
+		// Get name of the series.
+		public string Name (NChartSeries series)
+		{
+			return "My series";
+		}
+
+		public global::Android.Graphics.Bitmap Image (NChartSeries series)
+		{
+			return null;
+		}
+	}
+}
+
