af43a079c9f24b2402b2c17a78f7fb6a4c712dfc
erankatzshallev <eran@kimaia.com>
2015-06-16 17:40:52 +0300

    Logout
    logout button

--- Android/m2mAIRMobile/m2mAIRMobile/Source/Views/NavigationBarView.cs
+++ Android/m2mAIRMobile/m2mAIRMobile/Source/Views/NavigationBarView.cs
@@ -11,12 +11,15 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Shared.Utils;
+using Android.Source.Screens;
 
 namespace Android.Source.Views
 {
 	public class NavigationBarView : LinearLayout
 	{
-		private TextView navigationTitle;
+		private TextView 	navigationTitle;
+		private Button		logout;
 
 		public NavigationBarView (Context context) :
 			base (context)
@@ -41,11 +44,22 @@ namespace Android.Source.Views
 			LayoutInflater.From(context).Inflate(m2m.Android.Resource.Layout.nav_bar, this);
 
 			navigationTitle = FindViewById<TextView>(m2m.Android.Resource.Id.Navigation_Title);
+			logout = FindViewById<Button> (m2m.Android.Resource.Id.logout);
+			logout.Click += (object sender, EventArgs e) => { OnLogoutClicked(); };
 		}
 
 		public void SetTitle(string title)
 		{
 			navigationTitle.Text = title;
+		}
+
+		public void OnLogoutClicked()
+		{
+			Logger.Debug ("OnLogoutClicked()");
+			Settings.Instance.SetSessionId (null);
+			var intent = new Intent(this.Context, typeof(RegisterAndLoginActivity));
+			this.Context.StartActivity(intent);
+//			Finish ();
 		}
 	}
 }
